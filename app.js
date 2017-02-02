aecb54be69c1bec49d426040e48563e8029d2bee
choots <choots@workmail.com>
2017-02-01 22:26:54 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Untitled Dice v0.0.8
+// ProDice v0.0.8
 
 // Customize these configuration settings:
 
@@ -6,11 +6,12 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 18,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'ProDice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://untitled-dice.github.io',
+  recaptcha_sitekey: '6LfgCxQUAAAAAJJ8Pv5x2hP1NmKTuhPGQfgXwuPc
+',  // <----- EDIT ME!
+  redirect_uri: 'https://prodice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
